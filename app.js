11d4f97771714e5c897760f55118675eab2580e6
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-11-08 22:28:50 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/personalInfo", (req, res) => {
+  const personalInfo = require("./personalInfo.json");
+  res.json(personalInfo);
+});
+
+app.get("/education", (req, res) => {
+  const education = require("./education.json");
+  res.json(education);
+});
+
+app.get("/experience", (req, res) => {
+  const experience = require("./experience.json");
+  res.json(experience);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
