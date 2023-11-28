000581f264e446187108b29d188683051070a909
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-11-27 22:59:25 -0800

    fix typo in skills.json and add error handling/middleware

--- app.js
+++ app.js
@@ -2,28 +2,76 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(require("./middlewares/errorHandler"));
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 app.get("/personalInfo", (req, res) => {
-  const personalInfo = require("./personalInfo.json");
-  res.json(personalInfo);
+  try {
+    const personalInfo = require("./personalInfo.json");
+    res.json(personalInfo);
+  } catch (error) {
+    // file not found
+    if (error.code === "MODULE_NOT_FOUND") {
+      res.status(404).send("Personal info not found");
+    } else {
+      res.status(400).send("Bad Request");
+    }
+  }
 });
 
 app.get("/education", (req, res) => {
-  const education = require("./education.json");
-  res.json(education);
+  try {
+    const education = require("./education.json");
+    res.json(education);
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      res.status(404).send("Education not found");
+    } else {
+      res.status(400).send("Bad Request");
+    }
+  }
+});
+
+app.get("/skills", (req, res) => {
+  try {
+    const skills = require("./skills.json");
+    res.json(skills);
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      res.status(404).send("Skills not found");
+    } else {
+      res.status(400).send("Bad Request");
+    }
+  }
 });
 
 app.get("/experience", (req, res) => {
-  const experience = require("./experience.json");
-  res.json(experience);
+  try {
+    const experience = require("./experience.json");
+    res.json(experience);
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      res.status(404).send("Experience not found");
+    } else {
+      res.status(400).send("Bad Request");
+    }
+  }
 });
 
 app.get("/projects", (req, res) => {
-  const projects = require("./projects.json");
-  res.json(projects);
+  try {
+    const projects = require("./projects.json");
+    res.json(projects);
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      res.status(404).send("Projects not found");
+    } else {
+      res.status(400).send("Bad Request");
+    }
+  }
 });
 
 app.listen(port, () => {
